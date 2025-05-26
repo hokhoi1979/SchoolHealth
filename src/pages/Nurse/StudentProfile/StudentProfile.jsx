a5966857aba89a457b797559ae6be6fc31ddb8b1
ducky1103 <vuminhduc11032004@gmail.com>
2025-05-26 09:53:58 +0700

    provide medical manager

--- src/pages/Nurse/StudentProfile/StudentProfile.jsx
+++ src/pages/Nurse/StudentProfile/StudentProfile.jsx
@@ -1,13 +1,192 @@
-import React from "react";
+import React, { useState } from "react";
+import { AppFooter } from "../../../components/Footer/AppFooter";
+import CommonBreadcrumb from "../../../components/CommonBreadcrumb/CommonBreadcrumb";
+import { Link } from "react-router-dom";
+import { Button, Input, Space, Table, Tooltip } from "antd";
 
 const StudentProfile = () => {
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "id",
+      key: "id",
+      align: "center",
+    },
+    {
+      title: "Student",
+      dataIndex: "name",
+      key: "name",
+      align: "center",
+    },
+    {
+      title: "Grade",
+      dataIndex: "grade",
+      key: "grade",
+      align: "center",
+    },
+    {
+      title: "Date of Birth",
+      dataIndex: "date",
+      key: "date",
+      align: "center",
+    },
+    {
+      title: "Accident",
+      dataIndex: "accident",
+      key: "accident",
+      align: "center",
+    },
+
+    {
+      title: "Disease",
+      dataIndex: "disease",
+      key: "disease",
+      align: "center",
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+      key: "status",
+      align: "center",
+    },
+    {
+      title: "Action",
+      key: "action",
+      align: "center",
+      render: (_, record) => (
+        <Space>
+          <Tooltip
+            placement="bottom"
+            title="View"
+            overlayInnerStyle={{
+              fontFamily: "Poppins, sans-serif",
+              fontSize: "12px",
+            }}
+          >
+            <div style={{ cursor: "pointer" }}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width={20}
+                height={20}
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M12 9a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3m0 8a5 5 0 0 1-5-5a5 5 0 0 1 5-5a5 5 0 0 1 5 5a5 5 0 0 1-5 5m0-12.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5"
+                ></path>
+              </svg>
+            </div>
+          </Tooltip>
+        </Space>
+      ),
+    },
+  ];
+
+  const dataSource = [
+    {
+      id: "SE182629",
+      name: "Ho Khoi",
+      grade: "12A3",
+      date: "04/10/2004",
+      accident: "Fall",
+      disease: "Fever",
+      nameMedicine: "Paracetamol",
+      dosage: "1 tablet 500mg",
+      status: "Normal",
+    },
+    {
+      id: "SE182629",
+      name: "Ho Khoi",
+      grade: "12A3",
+      date: "04/10/2004",
+      accident: "Fall",
+      disease: "Fever",
+      nameMedicine: "Amoxicillin",
+      dosage: "1 tablet 500mg",
+      status: "Normal",
+    },
+    {
+      id: "SE182629",
+      name: "Ho Khoi",
+      grade: "12A3",
+      date: "04/10/2004",
+      accident: "Fall",
+      disease: "Fever",
+      nameMedicine: "Syringe 5ml",
+      dosage: "1 tablet 500mg",
+      status: "Special tracking",
+    },
+    {
+      id: "SE182629",
+      name: "Ho Khoi",
+      grade: "12A3",
+      date: "04/10/2004",
+      accident: "Fall",
+      disease: "Fever",
+      nameMedicine: "Vitamin C",
+      dosage: "1 tablet 500mg",
+      status: "Special tracking",
+    },
+    {
+      id: "SE182629",
+      name: "Ho Khoi",
+      grade: "12A3",
+      date: "04/10/2004",
+      accident: "Fall",
+      disease: "Fever",
+      nameMedicine: "Syringe 5ml",
+      dosage: "1 tablet 500mg",
+      status: "Special tracking",
+    },
+  ];
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Student Profile</h1>
-      <div className="bg-white rounded-lg shadow p-4">
-        {/* Add your student profile content here */}
+    <>
+      {" "}
+      <div className="flex flex-col min-h-screen">
+        <div className="p-6 flex flex-col flex-1">
+          <h1 className="text-xl font-inria font-medium mb-4">
+            <CommonBreadcrumb role={"Nurse"} page={"student"} />
+          </h1>
+
+          <div className="grid grid-cols-4 gap-5 mt-5 w-[100%] pl-5 pr-5 font-kameron ">
+            <div className="h-[120px] bg-white rounded-2xl">
+              <p className="flex justify-center mt-5">Total Student</p>
+              <p className="flex justify-center text-[50px]">800</p>
+            </div>
+            <div className="h-[120px] bg-white rounded-2xl">
+              <p className="flex justify-center mt-5">Sick student</p>
+              <p className="flex justify-center text-[50px]">12</p>
+            </div>
+            <div className="h-[120px] bg-white rounded-2xl">
+              <p className="flex justify-center mt-5">
+                Students needing special attention
+              </p>
+              <p className="flex justify-center text-[50px]">7</p>
+            </div>
+            <div className="h-[120px] bg-white rounded-2xl">
+              <p className="flex justify-center mt-5">Out of Stock</p>
+              <p className="flex justify-center text-[50px]">12</p>
+            </div>
+          </div>
+          <div className="pl-5 mt-5 flex gap-5">
+            <Input
+              style={{ borderRadius: "7px", width: "300px" }}
+              placeholder="Search for ID, Name student..."
+            />
+            <Button
+              className="!bg-[#90A8B0] !hover:bg-gray-600"
+              type="secondary"
+            >
+              <p className="text-white font-kameron"> Search</p>
+            </Button>
+          </div>
+          <Table className="mt-5" columns={columns} dataSource={dataSource} />
+        </div>
+
+        {/* Footer nằm dưới cùng */}
+        <AppFooter />
       </div>
-    </div>
+    </>
   );
 };
 
--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,35 +1,104 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 
-import RootLayout from "../layout/RootLayout";
-import NurseLayout from "../pages/Nurse/NurseLayout";
+
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
-import Dashboard from "../pages/Nurse/Dashboard/Dashboard";
-import Materials from "../pages/Nurse/Materials/Materials";
+import SideBar from "../components/SideBar/SideBarNurse";
+import Materials from "../pages/Manager/Materials/Materials";
+import NurseLayout from "../pages/Nurse/NurseLayout";
+import MedicineForStudent from "../pages/Manager/Materials/MedicineForStudent";
+import Export from "../pages/Manager/Materials/Export";
+import Import from "../pages/Manager/Materials/Import";
+import Inventory from "../pages/Manager/Materials/Inventory";
+import RootLayout from "../layout/RootLayout";
+import Dashboard from "../pages/Manager/Dashboard/dashboard";
+
+import Vaccination from "../pages/Manager/Dashboard/Vaccination";
+import Checkup from "../pages/Manager/Dashboard/Checkup";
+import Trend from "../pages/Manager/Dashboard/Trend";
 import StudentProfile from "../pages/Nurse/StudentProfile/StudentProfile";
+import Medical from "../pages/Manager/Dashboard/Medical";
 import MedicalEvent from "../pages/Nurse/Medical/MedicalEvent";
-import Vaccine from "../pages/Nurse/Vaccine/Vaccine";
-import MedicalCheckup from "../pages/Nurse/Medical/MedicalCheckup";
-import History from "../pages/Nurse/Medical/History";
+import Home from "../pages/Home/Home";
+import ManagerLayout from "../pages/Manager/ManagerLayout";
+import MedicalCheckup from "../pages/Manager/MedicalCheckup/MedicalCheckup";
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     children: [
-      { path: "", element: <Login /> },
-      { path: "/register", element: <Register /> },
+      { path: "", element: <Home /> },
+      {path: "/login",element:<Login/>},
+      {path: "/register",element:<Register/>},
       {
         path: "nurse",
         element: <NurseLayout />,
         children: [
-          { path: "", element: <Dashboard /> },
-          { path: "materials", element: <Materials /> },
-          { path: "student-profile", element: <StudentProfile /> },
-          { path: "medical-event", element: <MedicalEvent /> },
-          { path: "vaccine", element: <Vaccine /> },
-          { path: "medical-checkup", element: <MedicalCheckup /> },
-          { path: "history", element: <History /> },
+          { path: "", element: <Navigate to="dashboard" /> }, // chuyển hướng mặc định
+          {
+            path: "dashboard",
+            element: <Dashboard />,
+            children: [
+              { path: "", element: <Medical /> }, // index
+              { path: "vaccination", element: <Vaccination /> },
+              { path: "checkup", element: <Checkup /> },
+              { path: "trend", element: <Trend /> },
+            ],
+          },
+          {
+            path: "materials",
+            element: <Materials />,
+            children: [
+              { path: "", element: <Inventory /> },
+              { path: "import", element: <Import /> },
+              { path: "export", element: <Export /> },
+              { path: "medicine", element: <MedicineForStudent /> },
+            ],
+          },
+          {
+            path: "student",
+            element: <StudentProfile />,
+          },
+          {
+            path: "medical",
+            element: <MedicalEvent />,
+          },
+        ],
+      },
+      //
+      {
+        path: "/manager",
+        element: <ManagerLayout/>,
+        children: [
+          { path: "", element: <Navigate to="dashboard" /> }, // chuyển hướng mặc định
+          {
+            path: "dashboard",
+            element: <Dashboard />,
+            children: [
+              { path: "", element: <Medical /> }, // index
+              { path: "vaccination", element: <Vaccination /> },
+              { path: "checkup", element: <Checkup /> },
+              { path: "trend", element: <Trend /> },
+            ],
+          },
+          {
+            path: "materials",
+            element: <Materials />,
+            children: [
+              { path: "", element: <Inventory /> },
+              { path: "import", element: <Import /> },
+              { path: "export", element: <Export /> },
+              { path: "medicine", element: <MedicineForStudent /> },
+            
+            ],
+          },
+          {
+            path: "medicalcheckup",
+            element: <MedicalCheckup/>,
+          },
+          
         ],
       },
     ],
